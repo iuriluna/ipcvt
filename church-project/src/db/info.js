20afbc8a8b2608e47a27e0eb1df8ca9927edb0d6
araujo66 <adriano.araujo@enacom.com.br>
2023-03-11 11:05:54 -0300

    feat: creation info shared

---
+++ church-project/src/db/info.js
@@ -0,0 +1,6 @@
+export const about = `Deus nos ama e provê o caminho para a salvação. A nossa Redenção. Deus criou o mundo e a humanidade. A humanidade rebelou-se contra o Criador, escolhendo o pecado ao invés da obediência e isso fez separação entre nós e Deus. 
+    No entanto, Deus nos ama tanto que providenciou o caminho para a reconciliação. Jesus, que é Deus encarnado, veio à terra como ser humano, viveu aqui uma vida perfeita e sem pecar, morreu na cruz para pagar o preço pelo pecado da humanidade.
+    A salvação não é alcançada por esforço próprio, mas é um presente gratuito de Deus, dado por sua graça e amor, em Jesus.
+    E ao ressuscitar dentre os mortos, Jesus vence a morte e abre o caminho para a vida eterna, reservada aos que creem nEle.
+    Aqueles que creem no Evangelho são convidados a seguir a Jesus e a experimentar a reconciliação com Deus, vivendo uma vida transformada pela sua graça. 
+    Sua irresistível graça alcança nosso coração e o próprio Deus nos conduz em novidade de vida, preservando-nos para Ele até o último dia. Basta crer.`;
