27572c32b4257f6a16ef4c7d748bf60efb6f344a
araujo66 <adriano.araujo@enacom.com.br>
2023-03-13 08:07:45 -0300

    feat: functions to redirect

---
+++ church-project/src/routes/Cordination.js
@@ -0,0 +1,15 @@
+export const goToInitial = (navigate) => {
+  navigate('/');
+};
+
+export const goToAbout = (navigate) => {
+  navigate(`/sobre`);
+};
+
+export const goToDonation = (navigate) => {
+  navigate('/doe');
+};
+
+export const goToWhere = (navigate) => {
+  navigate(`/onde`);
+};
