31fa689747218f8af3ad4e970c9e0e460ce47b0a
araujo66 <adriano.araujo@enacom.com.br>
2023-03-13 08:00:19 -0300

    feat: index for views

---
+++ church-project/src/views/index.js
@@ -0,0 +1,4 @@
+export { default as Where } from './where';
+export { default as Home } from './home';
+export { default as About } from './about';
+export { default as Donation } from './donation';
