d4c3b3787dc5c91e5d9c073bb9f46591dff9845f
araujo66 <adriano.araujo@enacom.com.br>
2023-03-13 08:27:13 -0300

    feat: meu access to pages

--- church-project/src/components/Menu/index.jsx
+++ church-project/src/components/Menu/index.jsx
@@ -1,8 +1,16 @@
 import { useState } from 'react';
 import './styles.css';
+import { useNavigate } from 'react-router-dom';
 
 const Menu = () => {
-  return <h1>Menu</h1>;
+  const nav = useNavigate();
+  return (
+    <section className='menu'>
+      <h1 onClick={() => nav('/sobre')}>Sobre</h1>
+      <h1 onClick={() => nav('/onde')}>Onde</h1>
+      <h1 onClick={() => nav('/doe')}>Doe</h1>
+    </section>
+  );
 };
 
 export default Menu;
--- church-project/src/routes/Router.js
+++ church-project/src/routes/Router.js
@@ -1,28 +1,26 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { Routes, Route } from 'react-router-dom';
 import { Home, About, Donation, Where } from '../views/index';
 
 const Router = () => {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route
-          path='/'
-          element={<Home />}
-        />
-        <Route
-          path='/sobre'
-          element={<About />}
-        />
-        <Route
-          path='/doe'
-          element={<Donation />}
-        />
-        <Route
-          path='/onde'
-          element={<Where />}
-        />
-      </Routes>
-    </BrowserRouter>
+    <Routes>
+      <Route
+        path='/'
+        element={<Home />}
+      />
+      <Route
+        path='/sobre'
+        element={<About />}
+      />
+      <Route
+        path='/doe'
+        element={<Donation />}
+      />
+      <Route
+        path='/onde'
+        element={<Where />}
+      />
+    </Routes>
   );
 };
 
