96c0901259cd06ab876fb90c6bd8531e667ffa7c
araujo <theriancirculo@protonmail.com>
2023-04-19 16:12:24 -0300

    feat: integration youtube api

---
+++ church-project/src/service/BASE_URL.js
@@ -0,0 +1,1 @@
+export const BASE_URL = 'https://www.googleapis.com/youtube/v3/search?key=AIzaSyDLddmEjD2sv0e5M3LYeJUUpoe4-CHNwJg&channelId=UCTStCoaZBPHcZfp-c_RVqlg&part=snippet,id&order=date'
--- church-project/src/views/home/index.jsx
+++ church-project/src/views/home/index.jsx
@@ -1,9 +1,26 @@
-import { useState } from 'react';
+import { useState,useEffect } from 'react';
+import axios from "axios";
+
+import { BASE_URL } from '../../service/BASE_URL';
+
 import './styles.css'
 
 
-
 const Home = () => {
+
+  const [data, setData] = useState([])
+
+  const getData = ()=>{
+    axios.get(BASE_URL).then((response) => {
+      setData(response.data);
+    });
+  }
+
+  useEffect(() => {
+    getData()
+  }, []);
+
+
   return (
     <h1>home</h1>
   )
