65f73b0472ddad6c41bb812396257cad8a5e4f34
dev-araujo <theriancirculo@protonmail.com>
2023-04-22 08:09:56 -0500

    style: unused style

--- church-project/src/components/MainArea/index.jsx
+++ church-project/src/components/MainArea/index.jsx
@@ -4,11 +4,11 @@ import './styles.css'
 const MainArea = () => {
   return (
   <main>
-        <main>
-      <h1 className='h1-main' style={{paddingTop: '60px'}} >Acolhedora.</h1>
-      <h1 className='h1-main' >Transformadora.</h1>
+      
+      <h1  style={{paddingTop: '60px'}} >Acolhedora.</h1>
+      <h1 >Transformadora.</h1>
       <p>Comunicando a boa notícia da Redenção.</p>
-  </main>
+
   </main>
   )
 }
