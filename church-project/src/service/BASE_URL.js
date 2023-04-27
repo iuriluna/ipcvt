62f227b375062a86d7a2bf4c43f81df2247863b8
dev-araujo <theriancirculo@protonmail.com>
2023-04-27 10:56:26 -0300

    chore: config new api

--- church-project/src/service/BASE_URL.js
+++ church-project/src/service/BASE_URL.js
@@ -1,3 +1,2 @@
-import { REACT_APP_KEY } from "../config/config"
 
-export const BASE_URL = `https://www.googleapis.com/youtube/v3/search?key=${REACT_APP_KEY}&channelId=UCTStCoaZBPHcZfp-c_RVqlg&part=snippet,id&order=date`
+export const BASE_URL = `https://server-blog-dev.onrender.com/items`
--- church-project/src/views/home/index.jsx
+++ church-project/src/views/home/index.jsx
@@ -10,7 +10,7 @@ const Home = () => {
 
   const getData = () => {
     axios.get(BASE_URL).then((response) => {
-      setDataBase(response.data.items);
+      setDataBase(response.data);
     });
   };
 
@@ -19,30 +19,29 @@ const Home = () => {
   }, []);
 
   return (
-    <section className='container-videos-area' >
+    <section className="container-videos-area">
       {dataBase.map((video, index) => {
         return (
-          <section className='videos-area' >
-            <div className='layout-videos' >
-              <h2 className='title-videos' >{video.snippet.title}</h2>
-              <a 
+          <section key={index} className="videos-area">
+            <div className="layout-videos">
+              <h2 className="title-videos">{video.snippet.title}</h2>
+              <a
                 target="_blank"
-                rel='noreferrer' 
+                rel="noreferrer"
                 href={`https://www.youtube.com/watch?v=${video.id.videoId}`}
               >
-                <img 
-                  alt='video thumbnail'
+                <img
+                  alt="video thumbnail"
                   className="thumb"
-                  src={video.snippet.thumbnails.high.url} 
+                  src={video.snippet.thumbnails.high.url}
                 />
               </a>
             </div>
-            
           </section>
-        )
+        );
       })}
     </section>
-  )
-}
+  );
+};
 
 export default Home;
