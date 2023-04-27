f49e7dcc909e619094b701c50738f327012034f6
dev-araujo <theriancirculo@protonmail.com>
2023-04-27 11:14:33 -0300

    feat: load page

--- church-project/src/views/home/index.jsx
+++ church-project/src/views/home/index.jsx
@@ -1,16 +1,23 @@
 import { useState, useEffect } from "react";
 import axios from "axios";
 
+import Backdrop from "@mui/material/Backdrop";
+import CircularProgress from "@mui/material/CircularProgress";
+
 import { BASE_URL } from "../../service/BASE_URL";
 
 import "./styles.css";
 
 const Home = () => {
   const [dataBase, setDataBase] = useState([]);
+  const [load, setLoad] = useState(true);
 
   const getData = () => {
     axios.get(BASE_URL).then((response) => {
       setDataBase(response.data);
+      if (response.data.length > 0) {
+        setLoad(false);
+      }
     });
   };
 
@@ -40,6 +47,12 @@ const Home = () => {
           </section>
         );
       })}
+      <Backdrop
+        sx={{ color: "#fff", zIndex: (theme) => theme.zIndex.drawer + 1 }}
+        open={load}
+      >
+        <CircularProgress color="inherit" />
+      </Backdrop>
     </section>
   );
 };
